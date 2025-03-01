256ab60a2daf44f8bb8760adb5033aa57727cc18
Machuge27 <mutaihillary3912@gmail.com>
2025-03-01 12:57:08 +0300

    Dart week 4 to the cloud

--- express-server/src/utils/jwtUtils.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const SECRET_KEY = process.env.JWT_SECRET || 'your_secret_key';
-
-exports.generateToken = (payload) => {
-    return jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
-};
